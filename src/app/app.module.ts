9da0c02e181be48d385ec99d3281c893ff40fb1b
elis1386 <elis1386@gmail.com>
2023-02-08 14:52:41 +0200

    create firebase db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainScreenComponent } from './components/main-screen/main-screen.component';
 import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyDGLNwixKPkV0-aaKatIwNGuvC0TzAaJQw",
+        authDomain: "mom-shelper.firebaseapp.com",
+        projectId: "mom-shelper",
+        storageBucket: "mom-shelper.appspot.com",
+        messagingSenderId: "359984530723",
+        appId: "1:359984530723:web:5d3e5d5cd076c6d051b896"
+      }
+  };
